8888b5e736c10bb8105c37704a6bbac34b18e3ee
Rohit-theDeveloper <rohitsinghrathor237@gmail.com>
2025-11-11 17:42:48 +0530

    about change

--- src/Component/AboutUs.jsx
+++ src/Component/AboutUs.jsx
@@ -4,7 +4,6 @@ import { MdArrowOutward } from "react-icons/md";
 import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.jpg";
-import img5 from "../assets/img5.jpg";
 
 export default function AboutUs() {
   return (
